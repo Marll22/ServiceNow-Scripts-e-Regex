fb29fc10a1823a7dbf40f5e288a20cebed4bc48c
Alexandre Paduan <alepaduanlima@gmail.com>
2022-10-04 16:46:18 -0300

    dia 04.10

---
+++ Att case when task is att.js
@@ -0,0 +1,13 @@
+(function executeRule(current, previous /*null when async*/) {
+
+	var gr = new GlideRecord("sn_customerservice_task");
+	gr.addEncodedQuery("stateNOT IN3,5^u_case="+current.getValue("u_case"));
+	gr.query();
+	if (gr.getRowCount() == 0){
+		var caseOpen = new GlideRecord("sn_customerservice_case");
+		caseOpen.get(current.getValue("u_case"));
+		caseOpen.u_open_task = false;
+		caseOpen.update();
+	}	
+	
+})(current, previous);
